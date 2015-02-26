fc72d469fbde96a0fbc00ce952149841d1b0003b
Alex Medwid <alex.medwid@gmail.com>
2015-02-26 12:23:11 -0800

    Added the playTurn function, fixed the goal squares being initialized with 3 stones rather than zero.

--- Board.java
+++ Board.java
@@ -8,6 +8,43 @@ public class Board {
 	
 	
 	
+	public boolean playTurn( int player, int[] moves){//Returns whether or not that moveset was legal (attempting illegal moves
+		//This function plays out all of a player's moves on a particular turn.
+		
+		int movePosition = -1;//Start this at -1 since you increment it each time.
+		
+		do{
+			movePosition++;
+			
+			if(  !(canMakeMove(player, moves[movePosition])) ){//If the move is not a legal one
+				
+				return false;
+				
+			}
+			
+		}while( makeMove(player, moves[movePosition]) );//Keep going until you can't go again
+		
+		
+		return true;//If you haven't made an illegal move, then return true;].
+		
+	}
+	
+	public boolean canMakeMove( int player, int space){
+		if(space < 0 || space > 5){//Moves beyond the legal range
+			return false;
+		}
+		
+		if(player == 2){//Adjust the position for player 2
+			space += 7;
+		}
+		
+		if( spaces[space] == 0 ){
+			return false;
+		}
+		
+		return true;
+	}
+	
 	public boolean makeMove( int player, int space){//returns whether or not that move allows you to go again.
 		if( space < 0 || space > 5){//The move is out of the legal range
 			System.out.println("ERROR: Move outside of legal range.");
@@ -44,12 +81,12 @@ public class Board {
 		}while(spaces[currentLocation] != 1);//Keep moving until you place your last stone into an empty pit.
 		
 		if( player == 1 && currentLocation == 6 || player == 2 && currentLocation == 13){//If your last drop was into you home space
-			return true;
+			return true;//You can go again
 			
 		}
 		
 		else{
-			return false;
+			return false;//You can't go again
 		}
 		
 	}
@@ -73,6 +110,9 @@ public class Board {
 		for(int i = 0; i<spaces.length; i++){
 			spaces[i] = stoneCount;
 		}
+		
+		spaces[0] = 0;
+		spaces[spaces.length] = 0;//Set the two goal squares to zero.
 		
 	}
 	
